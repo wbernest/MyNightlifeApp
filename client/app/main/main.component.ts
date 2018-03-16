c7af8b42f6c62e22ce6fb4ea1c2aab8d1559572e
William Ernest <wbernest@gmail.com>
2018-03-16 18:32:19 -0400

    Got main page displaying bars in search area

--- client/app/main/main.component.ts
+++ client/app/main/main.component.ts
@@ -5,8 +5,8 @@ import routing from './main.routes';
 export class MainController {
   $http;
 
-  awesomeThings = [];
-  newThing = '';
+  searchResults = [];
+  search = '';
 
   /*@ngInject*/
   constructor($http) {
@@ -14,22 +14,35 @@ export class MainController {
 
   }
 
-  $onInit() {
-    this.$http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
+  $onInit(){
+    document.getElementById('searchInput').addEventListener('keypress',(evt) =>{
+      if (evt['keyCode'] == 13) {
+        this.searchYelp();
+      }
     });
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
+  accordionClick(index){
+    var collection = document.getElementsByClassName('in');
+    if(collection.length > 0 && collection[0].id == "collapse" + index) {
+      collection[0].className = 'panel-collapse collapse'
+    
+    }
+    else if(collection.length > 0){
+      collection[0].className = 'panel-collapse collapse'
+      document.getElementById('collapse' + index).className = 'panel-collapse collapse in';
+    }
+    else{
+      document.getElementById('collapse' + index).className = 'panel-collapse collapse in';
     }
   }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
+  searchYelp() {
+    this.$http.get('/api/yelps/' + this.search).then(response => {
+      this.searchResults = response.data.jsonBody.businesses;
+    }).catch(error => console.log(error));
   }
+  
 }
 
 export default angular.module('myNightlifeAppApp.main', [
--- server/routes.js
+++ server/routes.js
@@ -9,11 +9,18 @@ import path from 'path';
 
 export default function(app) {
   // Insert routes below
+  app.use('/api/yelps', require('./api/yelp'));
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
-
   app.use('/auth', require('./auth').default);
 
+  
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
