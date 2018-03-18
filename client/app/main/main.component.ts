1e35487177365a2d56f673dc2c3f9fd867a608d6
William Ernest <wbernest@gmail.com>
2018-03-18 13:22:22 -0400

    made query string work with page

--- client/app/main/main.component.ts
+++ client/app/main/main.component.ts
@@ -1,20 +1,28 @@
 const angular = require('angular');
 const uiRouter = require('angular-ui-router');
 import routing from './main.routes';
+import createHistory from 'history/createBrowserHistory';
+const queryString = require('query-string');
 
 export class MainController {
   $http;
 
   searchResults = [];
   search = '';
-
+  history: any;
+  parsed: any;
   /*@ngInject*/
   constructor($http) {
     this.$http = $http;
-
+    this.history = createHistory();
+    this.parsed = queryString.parse(location.search);
+    if(this.parsed.search!= undefined)
+      this.search = this.parsed.search;
   }
 
   $onInit(){
+    if(this.search != '') this.searchYelp();
+
     document.getElementById('searchInput').addEventListener('keypress',(evt) =>{
       if (evt['keyCode'] == 13) {
         this.searchYelp();
@@ -38,6 +46,18 @@ export class MainController {
   }
 
   searchYelp() {
+
+    if(this.parsed.search != this.search)
+    {
+      // If you prefer, use a single location-like object to specify both
+      // the URL and state. This is equivalent to the example above.
+      this.history.push({
+        pathname: "/",
+        search: "?search=" + this.search
+      });
+
+      this.parsed.search = this.search;
+    }
     this.$http.get('/api/yelps/' + this.search).then(response => {
       this.searchResults = response.data.jsonBody.businesses;
     }).catch(error => console.log(error));
